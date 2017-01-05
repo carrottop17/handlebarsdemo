b5df4a833b9783737e21cd1b8b37c1eb15b5d611
Daniel Barranco <danielbarranco@gmail.com>
2017-01-04 22:04:11 -0500

    add json file and gulp

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,23 @@
-var cast = {
-	"characters": [
-		{
-			"name": "Jon Snow",
-			"shortCode": "jon-snow",
-			"actor": "Kit Harrington",
-			"house": "Stark",
-			"location": "The Wall"
-		},
-		{
-			"name": "Tyrion Lannister",
-			"shortCode": "tyrion",
-			"actor": "Peter Dinklage",
-			"house": "Lannister"
-		},
-		{
-			"name": "Brienne of Tarth",
-			"shortCode": "brienne",
-			"actor": "Gwendoline Christie",
-			"house": "Clegane",
-			"location": "unknown"
-		},
-		{
-			"name": "Eddard Stark",
-			"shortCode": "ned-stark",
-			"actor": "Sean Bean",
-			"house": "Stark",
-			"location": "Winterfell"
-		},
-		{
-			"name": "Sandor Clegane",
-			"shortCode": "the-hound",
-			"actor": "Rory McCann",
-			"house": "Clegane",
-			"location": "unknown"
-		}
-	]
-}
 
 $(document).ready(function(){
 
 	var characterTemplate = $("#character-template").html();
 
 	var compiledCharacterTemplate = Handlebars.compile(characterTemplate);
-	$(".character-list-container").html(compiledCharacterTemplate(cast))
+
+	// console.log('ello');
+
+	// $.ajax("./data/cast.json").done(function(cast){
+	// 	console.log("done");
+	// })
+
+	$.ajax({
+					url: "cast.json",
+					        context: document.body,
+					        success: function(){
+					           alert("done");
+	            }});
+
+	// $(".character-list-container").html(compiledCharacterTemplate(cast))
 
 });
